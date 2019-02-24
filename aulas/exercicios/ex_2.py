833eac67cf39b783148dc2b679ed1e72dc0393c4
orochidrake <andrezaguette@gmail.com>
2019-02-24 18:11:45 -0300

    exerccios 2

---
+++ aulas/exercicios/ex_2.py
@@ -0,0 +1,30 @@
+num = int(input("Digite um Numero: "))
+print("")
+if num > 0:
+    print("O Numero é positivo")
+elif num < 0:
+    print("O Numero é negativo")
+else:
+    print("O Numero é zero")
+
+print("")
+
+num_2 = int(input("Digite um Numero: "))
+
+print("")
+
+if num_2 % 2 == 0:
+    print("O Numero é par")
+else:
+    print("O Numero é Impar")
+print("")
+
+num_3 = int(input("Digite um Numero: "))
+num_4 = int(input("Digite um Numero: "))
+print("")
+
+if num_3 > num_4:
+    print(num_3)
+else:
+    print(num_4)
+
